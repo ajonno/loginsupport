a3a1a3045837fbdb9dcbe061a1f6ed1ea27c388a
Angus Johnston <ajonno68@gmail.com>
2016-10-06 09:51:07 +1100

    added firebase

--- index.ios.js
+++ index.ios.js
@@ -1,8 +1,3 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
 
 import React, { Component } from 'react';
 import {
@@ -11,6 +6,7 @@ import {
   Text,
   View
 } from 'react-native';
+import * as firebase from 'firebase';
 
 class loginsupport extends Component {
   render() {
