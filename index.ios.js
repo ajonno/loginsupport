19f4871286e64112fa854e0a28f661bd31d65a82
Angus Johnston <ajonno68@gmail.com>
2016-10-06 11:50:36 +1100

    install Facebook’s React Native SDK (react-native install react-native-fbsdk)

--- index.ios.js
+++ index.ios.js
@@ -9,6 +9,56 @@ import {
 import * as firebase from 'firebase';
 
 class loginsupport extends Component {
+
+  constructor() {
+    super();
+
+    var config = {
+      apiKey: "AIzaSyB6RuTo566W-tRSZYwES4NghEPOa48gM7Q",
+      authDomain: "loginsupport-6f8f6.firebaseapp.com",
+      databaseURL: "https://loginsupport-6f8f6.firebaseio.com",
+      storageBucket: "loginsupport-6f8f6.appspot.com",
+      messagingSenderId: "322866779603"
+    };
+    firebase.initializeApp(config);
+
+    var provider = new firebase.auth.FacebookAuthProvider();
+    console.log(provider);
+
+    //part 1 - get the access token using Facebooks own SDK ?
+    var credential = firebase.auth.FacebookAuthProvider.credential(data.accessToken.toString());
+
+    //part 2 - use that token to sign in using Firebase's own API
+    firebase.auth().signInWithCredential(credential).then(function(user) {
+      console.log("Sign In Success", user);
+    }, function(error) {
+      console.log("Sign In Error", error);
+    });
+
+
+    // firebase.auth().signInWithCredential(provider).then(function(result) {
+    //   // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+    //   var token = result.credential.accessToken;
+    //   console.log("token: " + token);
+    //   // The signed-in user info.
+    //   var user = result.user;
+    //   // ...
+    // }).catch(function(error) {
+    //   // Handle Errors here.
+    //   var errorCode = error.code;
+    //   var errorMessage = error.message;
+    //   // The email of the user's account used.
+    //   var email = error.email;
+    //   // The firebase.auth.AuthCredential type that was used.
+    //   var credential = error.credential;
+    //   // ...
+    // });
+
+    //var credential = firebase.auth.FacebookAuthProvider.credential(
+    //            event.authResponse.accessToken);
+  }
+
+
   render() {
     return (
       <View style={styles.container}>
